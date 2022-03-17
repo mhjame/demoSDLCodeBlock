0cb8d044bca484be95274882fbbdb57b4a8d874d
Doan Thi Minh Hang <doanthiminhhangit@gmail.com>
2022-03-17 14:37:25 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <SDL.h>
+
+using namespace std;
+
+// int main(int arcg, char* argv[])
+
+int main(int, char**)
+{
+    cout << "Hello world!" << endl;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
+}
